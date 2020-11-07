aedf256cb8e23d2549e5d32a1a268258b5053acc
MillanUka <ukamillan@gmail.com>
2020-11-07 22:06:06 +1300

    Updates

---
+++ precache-manifest.5f2bef2f2cc46068b5029a0113855bf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6096f758efca522f1889f449db58a8f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8601d653ec1c8c7a884a",
+    "url": "/static/css/main.6fb97fb6.chunk.css"
+  },
+  {
+    "revision": "77d0579e67a7867ddaed",
+    "url": "/static/js/2.3c2dbbf1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3c2dbbf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8601d653ec1c8c7a884a",
+    "url": "/static/js/main.913b65d4.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
